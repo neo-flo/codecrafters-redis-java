a376302cd434ab1a6a1719553f4e0b6c66d6d869
노진산(Neo) <jinsan.noh@dreamus.io>
2023-03-17 10:37:43 +0900

    2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,10 +10,27 @@ public class Main {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
         final int port = 6379;
+        ServerSocket serverSocket = null;
         Socket clientSocket = null;
-        try (final ServerSocket serverSocket = new ServerSocket(port);) {
+        try {
+            serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept();
+
+//            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+//            StringBuilder result = new StringBuilder();
+//            for (int data = in.read(); data != -1; data = in.read()) {
+//                System.out.println("data = " + data);
+//                result.append((char) data);
+//            }
+
+//            String input = result.toString();
+//            System.out.println("input = " + input);
+
+            out.print("+PONG\r\n");
+            out.flush();
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
