31efc1327c2000ab4eb9df9613f289e7824ccd69
노진산(Neo) <jinsan.noh@dreamus.io>
2023-03-31 10:45:17 +0900

    4th

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,52 +5,67 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
         final int port = 6379;
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        try {
-            serverSocket = new ServerSocket(port);
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             serverSocket.setReuseAddress(true);
-            clientSocket = serverSocket.accept();
 
-            Scanner scanner = new Scanner(clientSocket.getInputStream());
-            OutputStream outputStream = clientSocket.getOutputStream();
-            StringBuilder input = new StringBuilder();
-
-            while (scanner.hasNext()) {
-                String next = scanner.nextLine();
-                System.out.println("next = " + next);
-                input.append(next);
-
-                if (next.startsWith("ping")) {
-                    String pong = "+PONG\r\n";
-                    outputStream.write(pong.getBytes());
-                } else if (next.startsWith("DOCS")) {
-                    outputStream.write("+\r\n".getBytes());
+            while (true) {
+                Socket clientSocket;
+                try {
+                    clientSocket = serverSocket.accept();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
+
+                new RedisConnection(clientSocket).start();
             }
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+    }
 
-            System.out.println("input = " + input);
+    public static class RedisConnection extends Thread {
 
-//            if (input.toString().contains("DOCS")) {
-//                out.println();
-//            } else {
-//                out.print("+PONG\r\n");
-//                out.flush();
-//            }
-//            out.flush();
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-        } finally {
+        private Socket clientSocket;
+
+        public RedisConnection(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
             try {
-                if (clientSocket != null) {
-                    clientSocket.close();
+                Scanner scanner = new Scanner(clientSocket.getInputStream());
+                OutputStream outputStream = clientSocket.getOutputStream();
+                StringBuilder input = new StringBuilder();
+
+                while (scanner.hasNext()) {
+                    String next = scanner.nextLine();
+                    System.out.println("next = " + next);
+                    input.append(next);
+
+                    if (next.startsWith("ping")) {
+                        String pong = "+PONG\r\n";
+                        outputStream.write(pong.getBytes());
+                    } else if (next.startsWith("DOCS")) {
+                        outputStream.write("+\r\n".getBytes());
+                    }
                 }
             } catch (IOException e) {
                 System.out.println("IOException: " + e.getMessage());
+            } finally {
+                try {
+                    if (clientSocket != null) {
+                        clientSocket.close();
+                    }
+                } catch (IOException e) {
+                    System.out.println("IOException: " + e.getMessage());
+                }
             }
         }
     }
